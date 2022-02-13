69a898438d80ae4e5d8ddbd461745535fdbe62a3
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-13 20:48:00 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './Components/landing/landing.component';
 import { ReposearchComponent } from './Components/reposearch/reposearch.component';
 import { QuerydisplayComponent } from './Components/querydisplay/querydisplay.component';
 
 
 const routes: Routes = [
-  {path:"user",component:QuerydisplayComponent},
-  {path:"repos",component:ReposearchComponent},];
+  {path:"home", component:LandingComponent},
+  { path: "user", component: QuerydisplayComponent },
+  { path: "repo", component: ReposearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
